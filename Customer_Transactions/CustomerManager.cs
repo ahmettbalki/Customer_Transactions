707a307250741326a7863e0d315608188d7bd79c
ahmettbalki <ahmettbalki@gmail.com>
2021-09-10 22:30:33 +0300

    Add project files.

---
+++ Customer_Transactions/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer_Transactions
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer added.");
+            Console.WriteLine("First Name : " + customer.FirstName + ". Last Name : " + customer.LastName + ". No: " + customer.CustomerNo);
+            Console.WriteLine("-------------------------");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted.");
+            Console.WriteLine("First Name : " + customer.FirstName + ". Last Name : " + customer.LastName + ". No: " + customer.CustomerNo);
+            Console.WriteLine("-------------------------");
+        }
+        public void List(Customer[] customer)
+        {
+            Console.WriteLine("Customers listed.");
+
+            foreach (var cust in customer)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("First Name : " + cust.FirstName);
+                Console.WriteLine("Last Name : " + cust.LastName);
+                Console.WriteLine("Customer No : " + cust.CustomerNo);
+                Console.WriteLine("-----------------------");
+            }
+        }
+    }
+}
---
+++ Customer_Transactions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Customer_Transactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.FirstName = "Ahmet";
+            customer1.LastName = "Balkı";
+            customer1.CustomerNo = "11";
+
+            Customer customer2 = new Customer();
+            customer2.FirstName = "Ezel";
+            customer2.LastName = "Bayraktar";
+            customer2.CustomerNo = "23";
+
+            Customer customer3 = new Customer();
+            customer3.FirstName = "Han";
+            customer3.LastName = "Yıldız";
+            customer3.CustomerNo = "52";
+
+            Customer customer4 = new Customer();
+            customer4.FirstName = "Burcu";
+            customer4.LastName = "Kızıl";
+            customer4.CustomerNo = "78";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.List(customers);
+            customerManager.Add(customer4);
+            customerManager.Delete(customer2);
+        }
+    }
+}
